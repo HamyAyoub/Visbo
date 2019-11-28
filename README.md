337fabeea42de2bfe36116d4cd6849b4844f979d
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-11-28 17:09:10 -0500

    fix: test using shallow rendering; updated readme

--- README.md
+++ README.md
@@ -3,21 +3,45 @@
 ## Features
 
 * React 16
+* * React
+* * ReactDOM
 * Webpack 4
-* Babel 7
-
-## Add-Ons
-
-* Hot Module Replacement
+* * Main components
+* * webpack - main engine of webpack plugin for its dependents to work
+* * webpack-cli - to access webpack commands through CLI (think npm start, npm build..)
+* * webpack-dev-server - client-side development server
+* * html-webpack-plugin - helps creating HTML templates
+* Babel 7 - transpiles JSX into JavaScript
+* * Main components:
+* * babel-core - main engine of babel plugin for its dependents to work
+* * babel-preset-env - the ES5, ES6 supporting part
+* * babel-preset-react - preset to react
+* * babel-loader - communicates with webpack
 * ESLint with Airbnb configuration
+* Redux - used to take control of your application’s state
+* Unit tests using Jest, Enzyme
+* PropTypes
+
+## Optional
+
 * React Dev Tools
-* Redux Dev Tools
+* Redux Dev Tools - allows you to see, play back, and change your action history
 
 ## Installation
 
 1. ```git clone https://github.com/alexandrakollarova/hello-world```
 2. ```cd hello-world-react-webpack-babel-setup```
-3. ```npm install```
-4. ```npm start```
-5. visit [http://localhost:3000](http://localhost:3000)
-6. build for production ```npm run build```
+3. ```npm install``` && ```npm start```
+4. visit [http://localhost:3000](http://localhost:3000)
+5. build for production ```npm run build```
+
+## Available Scripts in detail
+
+* npm start
+* * Webpack parses through the app starting at src/index.js, following any imported modules, until it has a complete dependency graph which then will be used to create a single JS file consisting of the app source code and modules used by the app, injects the file via script tag into public/index.html, and starts a development server on http://localhost:3000
+* npm test
+* * Jest testing framework handles unit testing here and it starts the test runner in 'watch' mode
+* npm build
+* * Webpack creates an optimized, production-ready bundle of the app, generates a new folder & files (./build/). The bundle created from this configuration has the source code minified and compressed for performance benefits and it also generates source maps
+* npm eject
+* * calling this command will remove the settings and all the nice stuff create-react-app provides for you
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,47 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from '../components/App';
 import HelloWorld from '../components/HelloWorld';
 import renderer from 'react-test-renderer';
-import Adapter from 'enzyme-adapter-react-16'
 import Enzyme, { shallow } from 'enzyme';
 import { Provider } from 'react-redux';
+import store from '../redux/store';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import configureStore from 'redux-mock-store';
 
-Enzyme.configure({ adapter: new Adapter() })
+configure({ adapter: new Adapter() })
 
-function setup() {
-  const props = {
-    text: jest.fn()
-  }
+// function shallowSetup() {
+//   const props = {
+//    text: "Hello World"
+//   }
 
-  const enzymeWrapper = shallow(<HelloWorld {...props} />)
+//   const enzymeWrapper = shallow(
+//     <Provider store={store}>
+//         <HelloWorld />
+//     </Provider>
+//   ).dive();
 
-  return {
-    props,
-    enzymeWrapper
-  }
-
-}
+//   return {
+//     props,
+//     enzymeWrapper
+//   };
+// }
 
 describe("HelloWorld Component", () => {
-  // it('renders without crashing using jest', () => {
-  //   const div = document.createElement('div');
-  //   ReactDOM.render(<App />, div);
-  //   ReactDOM.unmountComponentAtNode(div);
-  // });
 
-  // it('renders the UI as expected', () => {
-  //   const tree = renderer.create(<App />).toJSON();
-  //   expect(tree).toMatchSnapshot()
-  // });
+  it('renders without crashing using jest', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <Provider store={store}>
+        <HelloWorld />
+      </Provider>, 
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
-  it('should render self and subcomponents', () => {
-    const { enzymeWrapper } = setup()
+  it('renders the UI as expected', () => {
+    const tree = renderer.create(
+      <Provider store={store}>
+        <HelloWorld />
+      </Provider>
+      ).toJSON();
+    expect(tree).toMatchSnapshot()
+  });
 
-    expect(enzymeWrapper.find('h1').text()).toBe('Hello World')
+  it('should render an h1 tag containing Hello World string', () => {
+    //const { enzymeWrapper, props } = shallowSetup();
 
-    const helloWorldProps = enzymeWrapper.find('Hello World').props()
-    
-    expect(helloWorldProps.placeholder).toEqual('Hello World')
+    const enzymeWrapper = shallow(
+      <Provider store={store}>
+        <HelloWorld />
+      </Provider>
+    )
+
+    expect(enzymeWrapper.find('h1').text()).toBe(props.text);
+   // expect(enzymeWrapper.containsMatchingElement(<h1>Hello World</h1>)).toBe(true);
   });
 })
 
