0e490a04c534f32abfd527b056e97624c16ef409
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2020-01-19 17:58:31 -0500

    styling in progress, finish react functionality

--- src/components/LandingPage/Main.js
+++ src/components/LandingPage/Main.js
@@ -9,12 +9,14 @@ class Main extends Component {
         <div className='scrum-board-wrapper'>
           <img src={scrumBoard} alt="scrum-board-bg" className="scrum-board" />
         </div>
+
         <div className='landing-main-inner-wrapper'>
           <h1 className='headline'>Track small
             <br />to large scale
             <br />projects.
           </h1>
         </div>
+
         <div className='footer-wrapper'>
           <Link to='/demo-project' className='btn btn-demo'>EXPLORE VISBO</Link>
           <h3>Easy, flexibile and free.</h3>
