cc4cc5571b701ab612e3df0aa6dba49ae289ae91
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2020-01-29 20:03:01 -0500

    deploying

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  //API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://visbo-api.herokuapp.com/api',
   TOKEN_KEY: '',
 
 }
