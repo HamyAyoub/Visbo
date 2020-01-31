202199175cb04e20530fb48f05a28b0bf3d1784b
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2020-01-30 20:20:44 -0500

    figure out how to overrride react-trello styles

--- src/components/LandingPage/Main.js
+++ src/components/LandingPage/Main.js
@@ -6,17 +6,18 @@ class Main extends Component {
   render() {
     return (
       <main className='landing-main'>
-        <div className='scrum-board-wrapper'>
-          <img src={scrumBoard} alt="scrum-board-bg" className="scrum-board" />
-        </div>
+        <div className='headline-img-wrapper'>
+          <div className='scrum-board-wrapper'>
+            <img src={scrumBoard} alt="scrum-board-bg" className="scrum-board" />
+          </div>
 
-        <div className='landing-main-inner-wrapper'>
-          <h1 className='headline'>Track small
+          <div className='landing-main-inner-wrapper'>
+            <h1 className='headline'>Track small
             <br />to large scale
             <br />projects.
           </h1>
+          </div>
         </div>
-
         <div className='footer-wrapper'>
           <Link to='/demo-project' className='btn btn-demo'>EXPLORE VISBO</Link>
           <h3>Easy, flexibile and free.</h3>
